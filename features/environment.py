37611a2581d4f4b7417634ed17cad44323cd5e66
Sunjeet Khokhaer <sunjeet81@gmail.com>
2022-01-09 09:59:01 +1300

    added initial code behave with playwright

--- features/environment.py
+++ features/environment.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
 import os, json
+from playwright.sync_api import sync_playwright
 
-CONFIG_FILE = os.environ['CONFIG_FILE'] if 'CONFIG_FILE' in os.environ else 'config/single.json'
-TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
 
-with open(CONFIG_FILE) as data_file:
-    CONFIG = json.load(data_file)
-
-TB_KEY = os.environ['TB_KEY'] if 'TB_KEY' in os.environ else CONFIG['key']
-TB_SECRET = os.environ['TB_SECRET'] if 'TB_SECRET' in os.environ else CONFIG['secret']
+def before_all(context):
+    pass
 
 def before_feature(context, feature):
-    desired_capabilities = CONFIG['environments'][TASK_ID]
+    pass
+    
+        
+    '''desired_capabilities = CONFIG['environments'][TASK_ID]
 
     for key in CONFIG["capabilities"]:
         if key not in desired_capabilities:
@@ -20,7 +19,8 @@ def before_feature(context, feature):
     context.browser = webdriver.Remote(
         desired_capabilities=desired_capabilities,
         command_executor="https://%s:%s@hub.testingbot.com/wd/hub" % (TB_KEY, TB_SECRET)
-    )
+    )'''
 
 def after_feature(context, feature):
-    context.browser.quit()
+    '''context.browser.quit()'''
+    pass
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,10 +1,26 @@
 import time
+import environment
+import asyncio
+from playwright.sync_api import sync_playwright
+
+from features.environment import before_all
+
+@given('a Browser session is active')
+def initiate_browser(context):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context.page = browser.new_page()
+        context.page.goto('https://google.com')
 
 @when('visit url "{url}"')
-def step(context, url):
-    context.browser.get(url)
+def visit_page(context, url):
+    #this does not work ..event loop closed error
+    context.page.goto('https://example.com')
+    pass
+    
+    
 
-@when('field with name "{selector}" is given "{value}"')
+'''@when('field with name "{selector}" is given "{value}"')
 def step(context, selector, value):
     elem = context.browser.find_element_by_name(selector)
     elem.send_keys(value)
@@ -17,4 +33,4 @@ def step(context, title):
 
 @then(u'page contains "{body}"')
 def step(context, body):
-    assert body in context.browser.page_source
+    assert body in context.browser.page_source'''
